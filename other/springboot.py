0c3c7c4ef501a8377e076019db54f451edf9a3cb
xmm <xiongmengmeng@qipeipu.com>
2020-12-26 16:53:40 +0800

    1

--- other/springboot.py
+++ other/springboot.py
@@ -1,14 +1,12 @@
+import os,sys 
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+sys.path.insert(0,parentdir) 
+
 import xmind
-from xmind.core.const import TOPIC_DETACHED
 from xmind.core.markerref import MarkerId
-
-# load an existing file or create a new workbook if nothing is found
-w = xmind.load("c:\\Users\\btr\\Desktop\\springboot.xmind") 
-
-
-# create a new sheet 创建新工作表
-s2=w.createSheet()
-#s2=w.getPrimarySheet() 
+xmind_name="springboot"
+w = xmind.load(os.path.dirname(os.path.abspath(__file__))+"\\"+xmind_name+".xmind") 
+s2=w.createSheet() 
 s2.setTitle("springboot")
 r2=s2.getRootTopic()
 r2.setTitle("springboot")
@@ -100,64 +98,9 @@ content={
 
 }
 
-for key in content:
-    t1 = r2.addSubTopic()
-    t1.setTopicHyperlink(s2.getID()) 
-    t1.setTitle(key)
-    # print(content[key])
-    for i in content[key]:
-        # print(type(i))
-        if(type(i).__name__=='dict'):
-            for t in i:
-                t11 = t1.addSubTopic()
-                t11.setTopicHyperlink(t1.getID()) 
-                t11.setTitle(t)
-                for j in i[t]:
-                    #print(j)
-                    t111 = t11.addSubTopic()
-                    t111.setTopicHyperlink(t11.getID()) 
-                    t111.setTitle(j) 
-        else:
-            t11 = t1.addSubTopic()
-            t11.setTopicHyperlink(t1.getID()) 
-            t11.setTitle(i) 
 
 
-
-
-
-# Create a topic with a hyperlink 使用超链接创建主题
-# t2 = r2.addSubTopic()
-# t2.setTitle("事务")
-# #t2.setURLHyperlink("https://xmind.net") 
-
-# # Create a topic with notes 用笔记创建主题
-# t3 = r2.addSubTopic()
-# t3.setTitle("管道")
-# # t3.setPlainNotes("notes for this topic") 
-# # t3.setTitle("topic with \n notes")
-
-# # Create a topic with a file hyperlink
-# t4 = r2.addSubTopic()
-# #t4.setFileHyperlink("logo.jpeg") 
-# t4.setTitle("lua表达式")
-
-# # Create topic that is a subtopic of another topic
-# t41 = t1.addSubTopic()
-# t41.setTitle("string")
-
-# create a detached topic whose (invisible) parent is the root 创建一个分离的主题，其（不可见的）父级是根
-# d1 = r2.addSubTopic(topics_type = TOPIC_DETACHED)
-# d1.setTitle("detached topic")
-# d1.setPosition(0,20)
-
-# loop on the (attached) subTopics
-topics=r2.getSubTopics()
-# Demonstrate creating a marker 给节点增加图像
-for topic in topics:
-    topic.addMarker(MarkerId.starBlue)
-    
-
-
-# and we save
-xmind.save(w,"c:\\Users\\btr\\Desktop\\springboot.xmind") 
+#构建xmind
+xmind.build(content,r2)
+#保存xmind
+xmind.save(w,os.path.dirname(os.path.abspath(__file__))+"\\"+xmind_name+".xmind") 

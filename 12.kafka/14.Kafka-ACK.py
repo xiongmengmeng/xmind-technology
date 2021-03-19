f0b3cea1925cc38313682b761af15396f3da5431
xmm <xiongmengmeng@qipeipu.com>
2021-03-19 09:47:10 +0800

    kafka

---
+++ 12.kafka/14.Kafka-ACK.py
@@ -0,0 +1,22 @@
+import os,sys 
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+sys.path.insert(0,parentdir) 
+
+import xmind
+xmind_name="kafka"
+w = xmind.load(os.path.dirname(os.path.abspath(__file__))+"\\"+xmind_name+".xmind") 
+s2=w.createSheet()
+s2.setTitle("Kafka高并发")
+r2=s2.getRootTopic()
+r2.setTitle("Kafka高并发")
+
+
+content={
+页缓存技术 + 磁盘顺序写
+2、零拷贝技术
+}
+
+#构建xmind
+xmind.build(content,r2)
+#保存xmind
+xmind.save(w,os.path.dirname(os.path.abspath(__file__))+"\\"+xmind_name+".xmind") 
